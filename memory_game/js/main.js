f1151d633b3e4a16a83851afc7818291dba7f989
Adam Hwung <ahwung@Adams-MacBook-Pro.local>
2019-09-19 16:49:46 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+const cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
