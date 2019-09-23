68425fdaf5a4e5a5071d3aa08b5d01258e5dc057
Adam Hwung <ahwung@Adams-MacBook-Pro.local>
2019-09-23 13:30:41 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,12 +32,24 @@ function checkForMatch () {
 	}	
 }
 
-function flipCard (cardId) {
+function flipCard () {
+	cardId = this.getAttribute('data-id');
 	console.log( "User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);	
 	checkForMatch ();	
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
